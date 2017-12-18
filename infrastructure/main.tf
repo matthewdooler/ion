bf6098c1c3644af10f10caaa6bccfc8547b1288f
matthewdooler <matthew.dooler@sainsburys.co.uk>
2017-12-18 14:37:40 +0000

    lambda

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -46,3 +46,59 @@ resource "aws_sqs_queue_policy" "input_queue_sns_policy" {
 }
 POLICY
 }
+
+resource "aws_lambda_function" "ion" {
+    function_name = "ion"
+    handler = "server.run"
+    runtime = "python3.6"
+    filename = "server.zip"
+    source_code_hash = "${base64sha256(file("server.zip"))}"
+    role = "${aws_iam_role.ion_lambda_role.arn}"
+    timeout = 60
+}
+
+resource "aws_iam_role" "ion_lambda_role" {
+  name = "ion-lambda-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ion_lambda_role_policy" {
+  name = "ion-lambda-role-policy"
+  role = "${aws_iam_role.ion_lambda_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "sqs:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_sqs_queue.input_queue.arn}"
+    },
+    {
+      "Action": [
+        "sqs:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_sqs_queue.input_deadletter_queue.arn}"
+    }
+  ]
+}
+EOF
+}
---
+++ infrastructure/release.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd ../server && zip -r server.zip . && cp server.zip ../infrastructure && cd ../infrastructure
