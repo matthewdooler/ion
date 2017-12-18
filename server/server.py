b4a9955537b4b35a64eb2ca7e2911ee3af067fa4
matthewdooler <matthew.dooler@sainsburys.co.uk>
2017-12-18 13:40:43 +0000

    redrive dlq messages

--- server/server.py
+++ server/server.py
@@ -22,12 +22,34 @@ def send_email(server, port, user, pwd, recipient, subject, body):
 	server.sendmail(user, recipient, message)
 	server.close()
 
+def handle_send(message, message_json, email_send_delay, smtp_server, smtp_port, smtp_username, smtp_password):
+	recipient = message_json['recipient']
+	subject = message_json['subject']
+	body = message_json['body']
+	send_email(smtp_server, smtp_port, smtp_username, smtp_password, recipient, subject, body)
+	message.delete()
+	logging.info("Sent email")
+	time.sleep(email_send_delay)
+
+def handle_redrive(message, input_queue, deadletter_queue):
+	logging.info("Redriving DLQ...")
+	while True:
+		messages = deadletter_queue.receive_messages(MaxNumberOfMessages = 10)
+		for src_message in messages:
+			input_queue.send_message(MessageBody = src_message.body)
+			src_message.delete()
+		if len(messages) <= 0:
+			logging.info("Redrive finished")
+			break
+	message.delete()
+
 if __name__ == "__main__":
-	logging.basicConfig(format='%(asctime)s %(levelname)-s %(module)s:%(lineno)d - %(message)s', level=logging.INFO)
+	logging.basicConfig(format='%(asctime)s %(levelname)-s %(module)s:%(lineno)d - %(message)s', level = logging.INFO)
 
 	config = configparser.RawConfigParser()
 	config.read('config.cfg')
-	input_queue_name = config.get('sqs', 'queue_name')
+	input_queue_name = config.get('sqs', 'input_queue_name')
+	deadletter_queue_name = config.get('sqs', 'deadletter_queue_name')
 	sqs_read_delay = float(config.get('sqs', 'read_delay'))
 	email_send_delay = float(config.get('smtp', 'send_delay'))
 
@@ -39,22 +61,21 @@ if __name__ == "__main__":
 	smtp_password = config_secrets.get('smtp', 'password')
 
 	sqs = boto3.resource('sqs')
-	queue = sqs.get_queue_by_name(QueueName=input_queue_name)
+	input_queue = sqs.get_queue_by_name(QueueName=input_queue_name)
+	deadletter_queue = sqs.get_queue_by_name(QueueName=deadletter_queue_name)
 
 	while True:
 		logging.info("Polling SQS for messages...")
-		for message in queue.receive_messages(): # TODO: more messages per batch
+		for message in input_queue.receive_messages(MaxNumberOfMessages = 10):
 			try:
 				message_json = json.loads(message.body)
-				recipient = message_json['recipient']
-				subject = message_json['subject']
-				body = message_json['body']
-				send_email(smtp_server, smtp_port, smtp_username, smtp_password, recipient, subject, body)
-				message.delete()
-				logging.info("Sent email")
+				action = message_json['action']
+				if action == "send":
+					handle_send(message, message_json, email_send_delay, smtp_server, smtp_port, smtp_username, smtp_password)
+				elif action == "redrive":
+					handle_redrive(message, input_queue, deadletter_queue)
 			except Exception as e:
-				logging.info("Failed to send email: " + str(e))
-				logging.info("Original message from SQS: '" + message.body + "'")
+				logging.error("Error handling message: " + str(e) + ". Original message: '" + message.body + "'")
 				message.change_visibility(VisibilityTimeout = 60)
 			time.sleep(email_send_delay)
 		time.sleep(sqs_read_delay)
